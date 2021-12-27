3ffa4399c97d346ffb33c94226503d79f3ea5a5d
tgiqfe <tqji31001@gmail.com>
2021-12-27 15:22:43 +0900

    Delete GlobalSetting class.

--- plugin/OSVersion/Item.cs
+++ plugin/OSVersion/Item.cs
@@ -3,11 +3,15 @@ namespace OSVersion
 {
     internal class Item
     {
+        private static readonly string _WorkDir = Environment.UserName == "SYSTEM" ?
+            Path.Combine(Environment.GetEnvironmentVariable("ProgramData"), "WinBM") :
+            Path.Combine(Path.GetTempPath(), "WinBM");
+
         public const string OSVERSION_OSVERSIONINFODBFILE = "OSVersion_OSVersionInfoDBFile";
 
         public static string GetDefaultOSVersionInfoDbFile()
         {
-            return System.IO.Path.Combine(WinBM.GlobalSetting.WorkDir, "OSVersionInfoCollection.json");
+            return System.IO.Path.Combine(_WorkDir, "OSVersion", "OSVersionInfoCollection.json");
         }
     }
 }
