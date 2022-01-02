a0f857f5c53e57c5795c068d223618967957491f
tgiqfe <tqji31001@gmail.com>
2022-01-02 09:34:40 +0900

    Add IOTaskRequire class.

--- plugin/IO/Require/File/Owner.cs
+++ plugin/IO/Require/File/Owner.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinBM;
+using WinBM.Task;
+using IO.Lib;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
 
 namespace IO.Require.File
 {
-    internal class Owner
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+    internal class Owner : IOTaskRequire
     {
+        [TaskParameter(Mandatory = true, Resolv = true, Delimiter = ';')]
+        [Keys("path", "filepath", "target", "targetpath")]
+        protected string[] _Path { get; set; }
+
+        [TaskParameter(Mandatory = true, Resolv = true)]
+        [Keys("account", "acount", "owner", "own")]
+        protected string _Account { get; set; }
+
+        [TaskParameter]
+        [Keys("invert", "not", "no", "none")]
+        protected bool _Invert { get; set; }
+
+        private bool _abortRecurse = false;
+
+        public override void MainProcess()
+        {
+            Success = true;
+
+            _Account = PredefinedAccount.Resolv(_Account);
+
+            TargetFileProcess(_Path, OwnerFileAction);
+        }
+
+        private void OwnerFileAction(string target)
+        {
+            if (_abortRecurse) { return; }
+
+            bool ret = true;
+            var info = new System.IO.FileInfo(target);
+            try
+            {
+                FileSecurity security = info.GetAccessControl();
+                string owner = security.GetOwner(typeof(NTAccount)).Value;
+                ret &= owner.Equals(_Account, StringComparison.OrdinalIgnoreCase);
+            }
+            catch (Exception e)
+            {
+                Manager.WriteLog(LogLevel.Error, "{0} {1}", this.TaskName, e.Message);
+                Manager.WriteLog(LogLevel.Debug, e.ToString());
+                this.Success = false;
+            }
+
+            Success &= ret;
+        }
     }
 }
---
+++ plugin/IO/Require/IOTaskRequire.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO.Lib;
+using WinBM;
+using WinBM.Task;
+
+namespace IO.Require
+{
+    internal class IOTaskRequire : TaskJob
+    {
+        protected delegate void TargetFileAction(string path);
+
+        protected delegate void SrcDstFileAction(string source, string destination);
+
+        protected delegate void TargetDirectoryAction(string path);
+
+        protected delegate void SrcDstDirectoryAction(string source, string destination);
+
+        protected void TargetFileProcess(string[] paths, TargetFileAction targetFileAction)
+        {
+            foreach (string path in paths)
+            {
+                if (Path.GetFileName(path).Contains("*"))
+                {
+                    Manager.WriteLog(LogLevel.Info, "{0} Wildcard path.", this.TaskName);
+
+                    //  対象ファイルの親フォルダーが存在しない場合
+                    string parent = Path.GetDirectoryName(path);
+                    if (!System.IO.Directory.Exists(parent))
+                    {
+                        Manager.WriteLog(LogLevel.Warn, "Parent on target is Missing. \"{0}\"", parent);
+                        Success = false;
+                        continue;
+                        //  ↑returnにするかを検討中。恐らくこのままだが、一応変更する可能性があるのでコメントだけ残す。
+                    }
+
+                    //  ワイルドカード指定
+                    System.Text.RegularExpressions.Regex wildcard = Wildcard.GetPattern(path);
+                    System.IO.Directory.GetFiles(parent).
+                        Where(x => wildcard.IsMatch(x)).
+                        ToList().
+                        ForEach(x => targetFileAction(x));
+                }
+                else
+                {
+                    //  対象ファイルが存在しない場合
+                    if (!System.IO.File.Exists(path))
+                    {
+                        Manager.WriteLog(LogLevel.Warn, "Target is Missing. \"{0}\"", path);
+                        Success = false;
+                        continue;
+                        //  ↑returnにするかを検討中。恐らくこのままだが、一応変更する可能性があるのでコメントだけ残す。
+                    }
+
+                    targetFileAction(path);
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
