432380ba8108611f85deaa4b84019f6360b09b92
tgiqfe <tqji31001@gmail.com>
2022-01-02 09:15:50 +0900

    add (Require)IO/File/Owner.

---
+++ plugin/IO/Require/File/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.Require.File
+{
+    internal class Owner
+    {
+    }
+}
