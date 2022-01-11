7e87e132d0f79d1dd9d5284f0ac14285bfd7435c
tgiqfe <tqji31001@gmail.com>
2022-01-11 22:03:25 +0900

    Add sequential classes in IO.

---
+++ plugin/IO/Require/SequentialDirectory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.Require
+{
+    internal class SequentialDirectory : IOTaskRequire
+    {
+        //protected delegate void TargetDirectoryAction(string path);
+
+        //protected delegate void SrcDstDirectoryAction(string source, string destination);
+
+
+        /*
+         * ここにシーケンシャル用メソッドを実装予定
+         */
+    }
+}
---
+++ plugin/IO/Work/SequentialRegistry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using IO.Lib;
+
+namespace IO.Work
+{
+    internal class SequentialRegistry : IOTaskWork
+    {
+        //protected delegate void TargetRegistryKeyAction(RegistryKey key);
+
+        //protected delegate void TargetRegistryValueAction(RegistryKey key, string name);
+
+        //protected delegate void SrcDstRegistryKeyAction(RegistryKey sourceKey, RegistryKey destinationKey);
+
+        //protected delegate void SrcDstRegistryValueAction(RegistryKey sourceKey, RegistryKey destinationKey, string sourceName, string destinationName);
+
+
+        /*
+         * ここにシーケンシャル用メソッドを実装予定
+         */
+    }
+}
