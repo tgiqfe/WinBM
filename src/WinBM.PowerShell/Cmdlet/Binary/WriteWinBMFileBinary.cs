09bd5c1fb4876ed266b3e8b961f3bbdda20ecf89
tgiqfe <tqji31001@gmail.com>
2022-01-14 00:11:10 +0900

    Add Read-WinBMFileBinary.

---
+++ src/WinBM.PowerShell/Cmdlet/Binary/WriteWinBMFileBinary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Management.Automation;
+
+namespace WinBM.PowerShell.Cmdlet.Binary
+{
+    [Cmdlet(VerbsCommunications.Write, "WinBMFileBinary")]
+    public class WriteWinBMFileBinary : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string Text { get; set; }
+
+
+    }
+}
